555234d2b1d90152f4516b73a04eda62e229869d
VuQuyen <nvquyen@orients>
2018-05-03 16:35:55 +0700

    bundle to js

--- ClientApp/boot.browser.ts
+++ ClientApp/boot.browser.ts
@@ -2,7 +2,6 @@ import './polyfills/browser.polyfills';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module.browser';
-
 // // Enable either Hot Module Reloading or production mode
 if (module['hot']) {
     module['hot'].accept();
